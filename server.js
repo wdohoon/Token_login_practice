663cafa067d7f0162a6aa949d7b55150fe0e1aa2
원도훈 <dnjsehgns98@gmail.com>
2024-11-18 10:49:05 +0900

    Token

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:[본인의 포트번호]",
-      "http://localhost:[본인의 포트번호]",
+      "http://127.0.0.1:63342",
+      "http://localhost:63342",
     ],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
     credentials: true,
@@ -41,18 +41,39 @@ app.post("/", (req, res) => {
     res.status(401).send("로그인 실패");
   } else {
     // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
-    // 이곳에 코드를 작성하세요.
+    const accessToken = jwt.sign(
+      {
+        user_id: userInfo.user_id,
+        user_name: userInfo.user_name,
+      },
+      secretKey,
+      { expiresIn: "1h" } // 토큰 유효기간 설정
+    );
     // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
-    // 이곳에 코드를 작성하세요.
+    res.send(accessToken);
   }
 });
 
 // 클라이언트에서 get 요청을 받은 경우
 app.get("/", (req, res) => {
   // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
-  // 이곳에 코드를 작성하세요.
-  // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
-  // 이곳에 코드를 작성하세요.
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) {
+    return res.status(401).send("토큰이 없습니다.");
+  }
+
+  const token = authHeader.split(" ")[1]; // Bearer 토큰 형식에서 실제 토큰 값 추출
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).send("유효하지 않은 토큰입니다.");
+    }
+    // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
+    const userInfo = users.find((el) => el.user_id === decoded.user_id);
+    if (!userInfo) {
+      return res.status(404).send("유저 정보를 찾을 수 없습니다.");
+    }
+    res.send(userInfo);
+  });
 });
 
 app.listen(3000, () => console.log("서버 실행!"));
